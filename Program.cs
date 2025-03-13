2118ec7569b25287549a28cf19c0c9c432058b0b
nallely-lopez <lopezgarcializethnallely@gmail.com>
2025-03-12 20:33:58 -0600

    Se hizó referencia a la librería en los casos de switch

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -38,39 +38,39 @@ namespace Consola
                     {
                         case 1:
                             teclado1();
-                            Console.WriteLine("El resultado de la suma es: " + Class1.Sumar(num1, num2));
+                            Console.WriteLine("El resultado de la suma es: " + calculadoratectlaxiaco.Class1.Sumar(num1, num2));
                             break;
                         case 2:
                             teclado1();
-                            Console.WriteLine("El resultado de la Resta es: " + Class1.Restar(num1, num2));
+                            Console.WriteLine("El resultado de la Resta es: " + calculadoratectlaxiaco.Class1.Restar(num1, num2));
                             break;
                         case 3:
                             teclado1();
-                            Console.WriteLine("El resultado de la Multiplicacion es: " + Class1.Multiplicar(num1, num2));
+                            Console.WriteLine("El resultado de la Multiplicacion es: " + calculadoratectlaxiaco.Class1.Multiplicar(num1, num2));
                             break;
                         case 4:
                             teclado1();
-                            Console.WriteLine("El resultado de la Division es: " + Class1.Dividir(num1, num2));
+                            Console.WriteLine("El resultado de la Division es: " + calculadoratectlaxiaco.Class1.Dividir(num1, num2));
                             break;
                         case 5:
                             teclado0();
-                            Console.WriteLine("El resultado de la Raiz cuadrada es: " + Class1.RaizCuadrada(num0));
+                            Console.WriteLine("El resultado de la Raiz cuadrada es: " + calculadoratectlaxiaco.Class1.RaizCuadrada(num0));
                             break;
                         case 6:
                             teclado1();
-                            Console.WriteLine("El resultado de la Potencia es: " + Class1.Potencia(num1, num2));
+                            Console.WriteLine("El resultado de la Potencia es: " + calculadoratectlaxiaco.Class1.Potencia(num1, num2));
                             break;
                         case 7:
                             teclado1();
-                            Console.WriteLine("El resultado del porcentaje es: " + Class1.Porcentaje(num1, num2));
+                            Console.WriteLine("El resultado del porcentaje es: " + calculadoratectlaxiaco.Class1.Porcentaje(num1, num2));
                             break;
                         case 8:
                             teclado0();
-                            Console.WriteLine("El resultado de la fraccion es: " + Class1.Fraccion(num0));
+                            Console.WriteLine("El resultado de la fraccion es: " + calculadoratectlaxiaco.Class1.Fraccion(num0));
                             break;
                         case 9:
                             teclado0();
-                            Console.WriteLine("El resultado del Logaritmo Natural es: " + Class1.LogaritmoNatural(num0));
+                            Console.WriteLine("El resultado del Logaritmo Natural es: " + calculadoratectlaxiaco.Class1.LogaritmoNatural(num0));
                             break;
                         case 10:
                             Console.WriteLine("Has elegido salir de la aplicación");
